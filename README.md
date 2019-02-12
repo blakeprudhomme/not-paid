6f5a31b8c1af1d7f04de53e44ae07b406e06fff9
blakeprudhomme <blake.prudhomme@gmail.com>
2019-02-11 20:17:52 -0600

    Optional future developer notice
    Adds an optional notice for future developers.

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 ## Client did not pay?
 
 
-Add opacity to the body tag and decrease it every day until their site completely fades away. Set a due date and customize the number of days you offer them until the website is fully vanished. 
+Add opacity to the body tag and decrease it every day until their site completely fades away. Set a due date and customize the number of days you offer them until the website is fully vanished.
 
 
 ```javascript
 /* change these variables as you wish */
 var due_date = new Date('2017-02-27');
 var days_deadline = 60;
+var show_future_developer_notice = true;
 /* stop changing here */
 ```
 
@@ -23,4 +24,4 @@ A Wordpress plugin is also available [here](https://github.com/SurfEdge/not-paid
 Inspired from twitter (@riklomas)
 Made by Ciprian (@kleampa), Romania
 
-PS: No PR's or issues will be accepted for this project. 
+PS: No PR's or issues will be accepted for this project.
--- not-paid.js
+++ not-paid.js
@@ -1,24 +1,29 @@
 // Next time use letsdeel.com to make sure you get paid
 (function(){
 	/* change these variables as you wish */
-	var due_date = new Date('2017-02-27');
-	var days_deadline = 60;
+	const due_date = new Date('2017-02-27');
+	const days_deadline = 60;
+	const show_future_developer_notice = true;
 	/* stop changing here */
-	
-	var current_date = new Date();
-	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-	
+
+	const current_date = new Date();
+	const utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
+	const utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+	const days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+	const future_developer_notice =
+	  "NON-PAYMENT WARNING: The client's failure to pay on time has caused " +
+		"this website to fade into oblivion. Work for this client at your own risk."
+
 	if(days > 0) {
-		var days_late = days_deadline-days;
-		var opacity = (days_late*100/days_deadline)/100;
+		const days_late = days_deadline-days;
+		const opacity = (days_late*100/days_deadline)/100;
 			opacity = (opacity < 0) ? 0 : opacity;
 			opacity = (opacity > 1) ? 1 : opacity;
 		if(opacity >= 0 && opacity <= 1) {
 			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
+			if(show_future_developer_notice) {
+				console.log(future_developer_notice);
+			}
 		}
-		
 	}
-	
 })()
